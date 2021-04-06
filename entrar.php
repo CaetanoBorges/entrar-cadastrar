2c49511c139ad580d047c19c32de9eec7372b561
Caetano Borges <cbcaetanoborges@gmail.com>
2021-04-06 15:01:54 +0100

    primeiro

---
+++ entrar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Cadastro</title>
+    <link rel="stylesheet" href="entrar.css">
+</head>
+<style>
+*{margin:0;padding:0;}
+.corpo{width:700px;height:100vh;display:block;margin:0 auto;background:#eaeaea;position:relative;overflow-x:hidden;overflow-y:scroll}
+.navegacao{position:absolute;top:0;left:0;padding:10px;}
+.navegacao ul{list-style:none;}
+.navegacao ul li{display:inline-block;padding:10px;}
+</style>
+<body>
+    <div class="corpo">
+        <div class="navegacao">
+            <ul>
+                <li>ENTRAR</li>
+                <li>INSCREVER-SE</li>
+            </ul>
+        </div>
+        <div class="duasDivs">
+            
+        </div>
+    </div>
+</body>
+</html>
