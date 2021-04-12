d71bd10827a75ae118036da70d75411392a4c280
Caetano Borges <cbcaetanoborges@gmail.com>
2021-04-12 06:49:52 +0100

    de manha

--- entrar.php
+++ entrar.php
@@ -19,7 +19,7 @@
 /* TELAS DE LOGIN E CADASTRO */
 .duasDivs{width:100%;height:100%;display:block;}
 #entrar{width:100%;height:100%;background:red;align-items: center;display: flex;justify-content: center;}
-#inscrever{width:100%;height:100%;background:green;display:none;}
+#inscrever{width:100%;height:100%;background:green;align-items: center;justify-content: center;display:none;}
 
 /* TELA ENTRAR */
 .input-entrar{width:200px;height:30px;display:block;margin:2px auto;box-shadow: none;border: none;text-align: center;clear: both;}
@@ -30,8 +30,30 @@
 #esqueci-pass{display: none;}
 #obter-codigo{display: none;}
 #tenho-codigo{display: none;}
+
+.renovar{padding:10px;display: block;width: 60%;margin: 10px auto;}
+
+/*CADASTRAR `*/
+
+#cadastrar-nascimento{display: none;}
+#cadastrar-email{display: none;}
+#cadastrar-password{display: none;}
+#cadastrar-telefone{display: none;}
+#cadastrar-termos{display: none;}
+
+
+.acao-cadastrar{width:150px;height:30px;display:block;margin:2px auto;box-shadow: none;border: none;text-align: center;position: absolute;right:5px;bottom:5px;}
+.acao-cadastrar:focus{border: none;border-radius: 0;outline: none;box-shadow:none;}
+
 span{cursor: pointer;font-weight: bold;}
 button{cursor: pointer;}
+
+.dia-mes{width: 50px;height:30px;text-align:center;}
+.dia-mes:focus{outline: none;}
+
+.outline-none:focus{outline: none;}
+.genero{width:216px;height: 34px;text-align:center;margin-top:5px;}
+.mes{width:100px;height: 34px;text-align:center}
 </style>
 <body>
     <div class="corpo">
@@ -43,9 +65,59 @@ button{cursor: pointer;}
         </div>
         <div style="width:70%;display:block;margin:10vh auto;height:50%;position:relative">
             <div class="duasDivs">
+                <!-- -->
                 <div id="inscrever">
-                rbtyrty
+                    
+                    <div id="cadastrar-nome">
+                        <input type="text" placeholder="Nome próprio" class="input-entrar">
+                        <input type="text" placeholder="Apelido" class="input-entrar">
+                        <button class="acao-cadastrar" onclick='_paraHash("#cadastrar-nascimento")'>Seguinte</button>
+                    </div>
+                    <div id="cadastrar-nascimento">
+                        <input type="number" placeholder="Dia" class="dia-mes">
+                        <select name="" id="" class="mes outline-none">
+                            <option value="0">Mês</option>
+                            <option value="1">Janeiro</option>
+                            <option value="2">Fevereiro</option>
+                            <option value="3">Março</option>
+                            <option value="4">Abril</option>
+                            <option value="5">Maio</option>
+                            <option value="6">Junho</option>
+                            <option value="7">Julho</option>
+                            <option value="8">Agosto</option>
+                            <option value="9">Setembro</option>
+                            <option value="10">Outubro</option>
+                            <option value="11">Novembro</option>
+                            <option value="12">Dezembro</option>
+                        </select>
+                        <input type="number" placeholder="Ano" class="dia-mes">
+                        <br>
+                        <select name="" id="" class="genero outline-none">
+                            <option value="g">Género</option>
+                            <option value="m">Masculino</option>
+                            <option value="f">Feminino</option>
+                        </select>
+                        <button class="acao-cadastrar" onclick='_paraHash("#cadastrar-email")'>Seguinte</button>
+                    </div>
+                    <div id="cadastrar-email">
+                        <input type="email" placeholder="Email" class="input-entrar">
+                        <button class="acao-cadastrar" onclick='_paraHash("#cadastrar-password")'>Seguinte</button>
+                    </div>
+                    <div id="cadastrar-password">
+                        <input type="text" placeholder="Palavra passe" class="input-entrar">
+                        <button class="acao-cadastrar" onclick='_paraHash("#cadastrar-telefone")'>Seguinte</button>
+                    </div>
+                    <div id="cadastrar-telefone">
+                        <input type="text" placeholder="Número de telefone" class="input-entrar">
+                        <button class="acao-cadastrar" onclick='_paraHash("#cadastrar-termos")'>Seguinte</button>
+                    </div>
+                    <div id="cadastrar-termos">
+                        Termos
+                        <button class="acao-cadastrar" onclick=''>Concluir</button>
+                    </div>
                 </div>
+                <!-- -->
+                <!-- -->
                 <div id="entrar">
                     <div id="entrar-principal">
                         <input type="email" class="input-entrar">
@@ -54,21 +126,22 @@ button{cursor: pointer;}
                         <p class="esqueci-pass">Esqueci a palavra passe!<br> <span onclick='_paraHash("#esqueci-pass")'>Renovar</span></p>
                     </div>
                     <div id="esqueci-pass">
-                        <button class="input-entrar" onclick='_paraHash("#obter-codigo")'>Obter código para renovar a palavra passe</button>
-                        <button class="input-entrar" onclick='_paraHash("#tenho-codigo")'>Já tenho o código, quero renovar a palavra passe</button>
+                        <button class="renovar" onclick='_paraHash("#obter-codigo")'>Obter código para renovar a palavra passe</button>
+                        <button class="renovar" onclick='_paraHash("#tenho-codigo")'>Já tenho o código, quero renovar a palavra passe</button>
                     </div>
                     <div id="obter-codigo">
                         <p class="esqueci-sms">Insira o seu e-mail e irá receber um código <br> que ajudará a renovar a palavra passe.</p>
-                        <input type="email" class="input-entrar">
-                        <button class="input-entrar">Entrar</button>
+                        <input type="email" class="input-entrar" placeholder="Email">
+                        <button class="input-entrar">Receber código</button>
                     </div>
                     <div id="tenho-codigo">
                         <p class="esqueci-sms">Insira o e-mail e código que recebeu no e-mail<br>Para renovar a palavra passe.</p>
-                        <input type="email" class="input-entrar">
-                        <input type="text" class="input-entrar">
+                        <input type="email" class="input-entrar" placeholder="Email">
+                        <input type="text" class="input-entrar" placeholder="Código recebido">
                         <button class="input-entrar">Renovar Password</button>
                     </div>
                 </div>
+                <!-- -->
             </div>
         </div>  
     </div>
@@ -99,6 +172,18 @@ window.addEventListener("hashchange", function(e){
         routerEntrar(["none","none","none","block"])
     }
 
+    else if(location.hash == "#cadastrar-nascimento"){
+        routerCadastrar(["none","block","none","none","none","none"])
+    }else if(location.hash == "#cadastrar-email"){
+        routerCadastrar(["none","none","block","none","none","none"])
+    }else if(location.hash == "#cadastrar-password"){
+        routerCadastrar(["none","none","none","block","none","none"])
+    }else if(location.hash == "#cadastrar-telefone"){
+        routerCadastrar(["none","none","none","none","block","none"])
+    }else if(location.hash == "#cadastrar-termos"){
+        routerCadastrar(["none","none","none","none","none","block"])
+    }
+
 })
 
 _paraHash("#entrar");
@@ -116,8 +201,22 @@ function routerPrincipal(a) {
         _("#tenho-codigo").style.display = a[1];
         return
     }
-   _("#entrar").style.display = a[0];
-   _("#inscrever").style.display = a[1];
+    
+    if(a[1] == "flex"){
+        _("#inscrever").style.display = a[1];
+        _("#cadastrar-nome").style.display = "block";
+
+        
+        _("#entrar").style.display = a[0];
+        
+        _("#cadastrar-nascimento").style.display = a[0];
+        _("#cadastrar-email").style.display = a[0];
+        _("#cadastrar-password").style.display = a[0];
+        _("#cadastrar-telefone").style.display = a[0];
+        _("#cadastrar-termos").style.display = a[0];
+        return
+    }
+    
 }
 // FUNCAO ROUTER ENTRAR
 function routerEntrar(a) {
@@ -125,6 +224,15 @@ function routerEntrar(a) {
    _("#esqueci-pass").style.display = a[1];
    _("#obter-codigo").style.display = a[2];
    _("#tenho-codigo").style.display = a[3];
+}
+// FUNCAO ROUTER CADASTRAR
+function routerCadastrar(a) {
+   _("#cadastrar-nome").style.display = a[0];
+   _("#cadastrar-nascimento").style.display = a[1];
+   _("#cadastrar-email").style.display = a[2];
+   _("#cadastrar-password").style.display = a[3];
+   _("#cadastrar-telefone").style.display = a[4];
+   _("#cadastrar-termos").style.display = a[5];
 }
 </script>
 </html>
