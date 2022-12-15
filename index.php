01b06d6e893f1db69c7496618559f3029ccc4f2a
Caetano Borges <cbcaetanoborges@gmail.com>
2022-12-15 23:09:38 +0100

    com cdn

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entrar - Cadastrar</title>
     <link rel="stylesheet" href="passtrength.css">
-    <link rel="stylesheet" href="entrar.css">
-    <link rel="shortcut icon" href="ico.png" type="image/x-icon">
+    <link rel="stylesheet" href="http://cdn.binga.ao/entrar/entrar.css">
+    <link rel="shortcut icon" href="http://cdn.binga.ao/conta/ico.png" type="image/x-icon">
 </head>
 <style>
 
@@ -21,19 +21,20 @@
             </ul>
         </div>
         <div id="todocorpo">
+            <br><br><br>
             <style>
                 .load{position: absolute; top: 1px; left:1px; width:100%; height: 100%;background: #fff;display: none;}
                 .load img {width: 30%; position: absolute; top: 40%; left:35%;}
             </style>
             <div class="load">
-                <img src="logo.gif" alt="">
+                <img src="http://cdn.binga.ao/conta/logo.gif" alt="">
             </div>
             <div class="duasDivs">
                 <!-- -->
                 <div id="inscrever">
-                    <img src="voltar.png" class="voltar" onclick="history.back()">
+                    <img src="http://cdn.binga.ao/entrar/voltar.png" class="voltar" onclick="history.back()">
                     <div id="cadastrar-nome">
-                        <img src="logo.png">
+                        <img src="http://cdn.binga.ao/conta/logo.png">
                         <h2>Criar uma conta</h2>
                         <div class="div-centro">
                             <p>Introduza o seu nome</p>
@@ -42,13 +43,13 @@
                         <input type="text" placeholder="Apelido" class="input-entrar bckgrnd-input" id="iapelido">
                         <div class="div-centro">
                             <div class="erro">
-                                <p class="sms-erro"></p><img src="erro.webp" class="icon-erro">
+                                <p class="sms-erro"></p><img src="http://cdn.binga.ao/conta/erro.webp" class="icon-erro">
                             </div>
                         </div>
                         <button class="acao-cadastrar bckgrnd-button-c" onclick='_iNomeJa()'>Seguinte</button>
                     </div>
                     <div id="cadastrar-nascimento">
-                        <img src="logo.png">
+                        <img src="http://cdn.binga.ao/conta/logo.png">
                         <h2>Informações básicas</h2>
                         <div class="div-centro">
                             <p>Introduzir a sua data de nascimento e o seu género</p><br>
@@ -78,13 +79,13 @@
                         </div>
                         <div class="div-centro">
                             <div class="erro">
-                                <p class="sms-erro"></p><img src="erro.webp" class="icon-erro">
+                                <p class="sms-erro"></p><img src="http://cdn.binga.ao/conta/erro.webp" class="icon-erro">
                             </div>   
                         </div>
                         <button class="acao-cadastrar bckgrnd-button-c" onclick='_iNascimentoJa()'>Seguinte</button>
                     </div>
                     <div id="cadastrar-email">
-                        <img src="logo.png">
+                        <img src="http://cdn.binga.ao/conta/logo.png">
                         <h2>Deve adicionar<br>o seu email à conta!</h2>
                         <input type="email" placeholder="Email" class="input-entrar bckgrnd-input" id="iEmail">
                         <br>
@@ -94,13 +95,13 @@
                         </div>
                         <div class="div-centro">
                             <div class="erro">
-                                <p class="sms-erro"></p><img src="erro.webp" class="icon-erro">
+                                <p class="sms-erro"></p><img src="http://cdn.binga.ao/conta/erro.webp" class="icon-erro">
                             </div>   
                         </div>
                         <button class="acao-cadastrar bckgrnd-button-c" onclick='_iEmailJa()'>Seguinte</button>
                     </div>
                     <div id="cadastrar-password">
-                        <img src="logo.png">
+                        <img src="http://cdn.binga.ao/conta/logo.png">
                         <h2>Criar uma<br>palavra-passe forte</h2>
                         <div class="div-centro">
                             <p>Crie uma plavra passe forte com uma mistura de letras, números e símbolos.</p>
@@ -109,13 +110,13 @@
                         <input type="password" placeholder="Palavra-passe" class="input-entrar bckgrnd-input" id="iPass">
                         <div class="div-centro">
                             <div class="erro">
-                                <p class="sms-erro"></p><img src="erro.webp" class="icon-erro">
+                                <p class="sms-erro"></p><img src="http://cdn.binga.ao/conta/erro.webp" class="icon-erro">
                             </div>   
                         </div>
                         <button class="acao-cadastrar bckgrnd-button-c" onclick='_iPassJa()'>Seguinte</button>
                     </div>
                     <div id="cadastrar-telefone">
-                        <img src="logo.png">
+                        <img src="http://cdn.binga.ao/conta/logo.png">
                         <h2>Pretende adicionar<br>o número de telefone à conta?</h2>
                         <input type="number" placeholder="Número de telefone" class="input-entrar bckgrnd-input" id="iTelefone">
                         <br>
@@ -125,7 +126,7 @@
                         <button class="acao-cadastrar bckgrnd-button-c" onclick='_iTelefoneJa()'>Seguinte</button>
                     </div>
                     <div id="cadastrar-termos">
-                        <img src="logo.png">
+                        <img src="http://cdn.binga.ao/conta/logo.png">
                         <h2>Leia atentamente</h2>
                         <p class="termo">1- Esta conta te dá acesso a todos serviços da nossa plataforma, sejam os de distribuição livre,
                         sejam os que precisa de um pacote de subscrição. Os serviços da plataforma são mobile, web, e desktop.</p>
@@ -140,7 +141,7 @@
                 <!-- -->
                 <div id="entrar">
                     <div id="entrar-principal">
-                        <img src="logo.png" alt="">
+                        <img src="http://cdn.binga.ao/conta/logo.png" alt="">
                         <h2>Damos-lhe<br>as boas-vindas</h2>
                         <input type="email" class="input-entrar" id="email-entrar">
                         <input type="password" class="input-entrar"  id="pass-entrar">
@@ -155,15 +156,15 @@
                         </div>
                     </div>
                     <div id="esqueci-pass">
-                        <img src="voltar.png" class="voltar" onclick="history.back()">
-                        <img src="logo.png" alt="">
+                        <img src="http://cdn.binga.ao/entrar/voltar.png" class="voltar" onclick="history.back()">
+                        <img src="http://cdn.binga.ao/conta/logo.png" alt="">
                         <h2>Recuperação<br>de conta</h2>
                         <button class="renovar bckgrnd-button-c" onclick='_paraHash("#obter-codigo")'>Obter código para renovar a palavra-passe</button>
                         <button class="renovar bckgrnd-button-e" onclick='_paraHash("#tenho-codigo")'>Já tenho o código, quero renovar a palavra-passe</button>
                     </div>
                     <div id="obter-codigo">
-                        <img src="voltar.png" class="voltar" onclick="history.back()">
-                        <img src="logo.png" alt="">
+                        <img src="http://cdn.binga.ao/entrar/voltar.png" class="voltar" onclick="history.back()">
+                        <img src="http://cdn.binga.ao/conta/logo.png" alt="">
                         <div class="div-centro">
                             <p class="esqueci-sms">Insira o seu e-mail e irá receber um código <br> que ajudará a renovar a palavra-passe.</p>
                         </div>
@@ -173,14 +174,14 @@
                         <div class="div-centro">
                             <div class="codigo-renova-erro">
                                 <div class="erro">
-                                    <p class="sms-erro"></p><img src="erro.webp" class="icon-erro">
+                                    <p class="sms-erro"></p><img src="http://cdn.binga.ao/conta/erro.webp" class="icon-erro">
                                 </div>   
                             </div>   
                         </div>
                     </div>
                     <div id="tenho-codigo">
-                        <img src="voltar.png" class="voltar" onclick="history.back()">
-                        <img src="logo.png" alt="">
+                        <img src="http://cdn.binga.ao/entrar/voltar.png" class="voltar" onclick="history.back()">
+                        <img src="http://cdn.binga.ao/conta/logo.png" alt="">
                         <div class="div-centro">
                             <p class="esqueci-sms">Insira o e-mail e código que recebeu no e-mail<br>Para renovar a palavra-passe.</p>
                         </div>
@@ -191,14 +192,14 @@
                         <div class="div-centro">
                             <div class="passe-renova-erro">
                                 <div class="erro">
-                                    <p class="sms-erro"></p><img src="erro.webp" class="icon-erro">
+                                    <p class="sms-erro"></p><img src="http://cdn.binga.ao/conta/erro.webp" class="icon-erro">
                                 </div>   
                             </div>   
                         </div>
                     </div>
                     <div id="nova-palavra-passe">
-                        <img src="voltar.png" class="voltar" onclick="history.back()">
-                        <img src="logo.png" alt="">
+                        <img src="http://cdn.binga.ao/entrar/voltar.png" class="voltar" onclick="history.back()">
+                        <img src="http://cdn.binga.ao/conta/logo.png" alt="">
                         <div class="div-centro">
                             <p class="esqueci-sms">Insira a sua palavra-passe nova.</p>
                         </div>
@@ -207,7 +208,7 @@
                         <div class="div-centro">
                             <div class="passe-nova-erro">
                                 <div class="erro">
-                                    <p class="sms-erro"></p><img src="erro.webp" class="icon-erro">
+                                    <p class="sms-erro"></p><img src="http://cdn.binga.ao/conta/erro.webp" class="icon-erro">
                                 </div>   
                             </div>   
                         </div>
@@ -218,10 +219,10 @@
         </div>  
     </div>
 </body>
-<script src="jquery.js" ></script>
+<script src="http://cdn.binga.ao/_bibliotecas/jquery/jquery.js" ></script>
 <script src="passtrength.js"></script>
-<script src="funcoesGenericas.js"></script>
-<script src="router.js"></script>
+<script src="http://cdn.binga.ao/entrar/funcoesGenericas.js"></script>
+<script src="http://cdn.binga.ao/entrar/router.js"></script>
 <?php
 $servidor = "http://apiconta.binga.ao";
 $urlDaRequisicao = $_SERVER['REQUEST_URI'];
@@ -243,7 +244,7 @@ $('#iPass, #palavra-passe').passtrength({
   textMedium: "Media",
   textStrong: "Forte",
   textVeryStrong: "Muito Forte",
-  eyeImg : "eye.svg"
+  eyeImg : "http://cdn.binga.ao/entrar/eye.svg"
 });
 
 function receberNumeroRecuperacao(){
@@ -323,7 +324,7 @@ function novaPalavraPasse(){
             var obj = JSON.parse(response);
             if (obj.ok) {
                 _erroInput(".passe-nova-erro", "Alterou a palavra passe com sucesso.", 1);
-                location.href = "http://127.0.0.1/binga/conta-ui/inicio.php?ftpadbc="+obj.payload;//"https://conta.binga.ao";
+                location.href = "http://conta.binga.ao/inicio.php?ftpadbc="+obj.payload;//"https://conta.binga.ao";
             } else {
                 _erroInput(".passe-nova-erro", obj.payload, 1);
                 setTimeout(() => {
@@ -338,6 +339,6 @@ function novaPalavraPasse(){
     
 }
 </script>
-<script src="preenchimentoFormInscrever.js"></script>
+<script src="http://cdn.binga.ao/entrar/preenchimentoFormInscrever.js"></script>
 
 </html>
